b9ebc1b5865c4da0b30cbda2c92b14ef01539706
Yaovi Avoudikpon <yaovi.avoud@gmail.com>
2023-08-02 21:39:51 +0100

    AddedEndpointCodeReact

--- react-app/src/services/PetServices.js
+++ react-app/src/services/PetServices.js
@@ -1,8 +1,24 @@
-import { hostNameUrl } from "../config/api";
-import axios from "axios";
+//import { hostNameUrl } from "../config/api";
+//import axios from "axios";
+import http from './axios-common';
 
 
-export const findPets = () => {
-  const pets = axios.get(`${hostNameUrl}/dogs`);
-  return pets;
-};
+export function saveUser(user){
+  return http.post("/user", user)
+}
+
+
+// export const findPets = () => {
+//   const pets = axios.get(`${hostNameUrl}/dogs`);
+//   return pets;
+// };
+
+// import http from './axios-common';
+
+// export function getAllHeroes(){
+//     return http.get("/heroes");
+// }
+
+// export function saveHero(hero){
+//     return http.post("/heroes", hero)
+// }
