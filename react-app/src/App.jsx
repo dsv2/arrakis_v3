b9926c8ae8dd3e3e92d42083b399db4e326c7116
Yaovi Avoudikpon <yaovi.avoud@gmail.com>
2023-08-02 18:41:52 +0100

    first commit

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pets } from "./components/pets/Pets";
+import { Login } from "./components/pets/login";
+import { Register } from "./components/register";
 
-const App = () => {
-  return <Pets />;
-};
+
+// const App = () => {
+//   return <Pets />;
+// };
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
 
 export default App;
