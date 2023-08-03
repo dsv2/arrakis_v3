6e252efd508938914f9d182d265d15f93b20d1a1
Zihao Fang <fzhprivate@gmail.com>
2023-08-03 17:12:59 +0100

    Finished populating BOOK

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -3,3 +3,9 @@ VALUES
 ('divya', 'password123'),
 ('lily', 'password321'),
 ('minahil', 'password098');
+
+DROP TABLE full_data IF EXISTS;
+CREATE TABLE  full_data  AS SELECT * FROM CSVREAD('C:\Users\Admin\Downloads\db-bonds-data.csv');
+
+INSERT INTO BOOK(NAME)
+SELECT BOOK_NAME FROM full_data;
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -6,13 +6,16 @@
 --    age INT NOT NULL
 --);
 
---DROP TABLE IF EXISTS BOOKS;
---
---CREATE TABLE BOOKS
---AS SELECT BOOK_NAME, ISIN, BOND_HOLDER, ISSUER_NAME, BOND_MATURITY_DATE
---FROM FULLDATA;
---
---SELECT * FROM BOOKS;
+DROP SCHEMA IF EXISTS `bonds`;
+CREATE SCHEMA `bonds`;
+use bonds;
+
+CREATE TABLE `book` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+--ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS users;
 
@@ -20,4 +23,62 @@ CREATE TABLE users (
     username VARCHAR(25) NOT NULL,
     password VARCHAR(25) NOT NULL
 );
+
+CREATE TABLE `counterparty` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+--ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--CREATE TABLE `book_user` (
+--  `book_id` int NOT NULL,
+--  `user_id` int NOT NULL,
+--  KEY `FK1_book_id` (`book_id`),
+--  KEY `FK_user_id` (`user_id`)
+--) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `security` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `isin` varchar(50) DEFAULT NULL,
+  `cusip` varchar(50) DEFAULT NULL,
+  `issuer_name` varchar(255) NOT NULL,
+  `maturity_date` datetime NOT NULL,
+  `coupon` float NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `face_value` float NOT NULL,
+  `currency` varchar(10) NOT NULL,
+  `status` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+--ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `trades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_id` int NOT NULL,
+  `security_id` int NOT NULL,
+  `counterparty_id` int NOT NULL,
+  `currency` varchar(10) NOT NULL,
+  `status` varchar(32) NOT NULL,
+  `quantity` int NOT NULL,
+  `unit_price` float NOT NULL,
+  `buy_sell` varchar(32) NOT NULL,
+  `trade_date` datetime NOT NULL,
+  `settlement_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+--  KEY `FK_security_id` (`security_id`),
+--  KEY `FK_counterparty_id` (`counterparty_id`),
+--  KEY `FK_book_id` (`book_id`)
+);
+--ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--ALTER TABLE book_user ADD PRIMARY KEY(book_id, user_id);
+
+
+
+
+
+
 
