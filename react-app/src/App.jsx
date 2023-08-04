c3898a5e6517fbde8e351017c3e5d5af17892d60
Minahil Chaudhary <minahilqamar@hotmail.com>
2023-08-04 17:25:41 +0100

    connecting to backend

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -8,47 +8,59 @@ import { useState } from 'react';
 
 
 function App() {
-  const [loginSuccess, setLoginStatus] = useState(false);
 
-  return (
-  <div className="App">
-        {/* <div className="App">
-          { <h1>Login Page</h1> }
-          <LoginForm />
-        </div> */}
-        
-        <div>
-        {
-          
-          !loginSuccess ?
-          <LoginForm loginSuccess = {setLoginStatus}/>
-          :<BondsPage/>
-        
-        }
-        </div>
-        
-        
-       
-       
-        {/* if (loginSuccess) {
+    const [loginSuccess, setLoginStatus] = useState(false);
+
+    const handleLoginSuccess = fetch("localhost:3030/UserController/checkUser", {
+        method: "POST"
+
+    })
+    if (handleLoginSuccess) {
+        setLoginStatus(true);
+    } else {
+        setLoginStatus(false)
+    }
+
+    //setLoginStatus(checkUser=true);
+
+
+    return (
+        <div className="App">
+            <div className="App">
+                <h1>Login Page</h1>
+                {!loginSuccess ? <LoginForm
+                    LoginSuccess={handleLoginSuccess}
+                /> : <BondsPage />}
+            </div>
+
+            <div>
+                {
+
+                }
+            </div>
+
+
+
+
+            {/* if (loginSuccess) {
           return <BondsPage />;
         }
         return <LoginForm />; */}
 
-        
-  </div>
+
+        </div>
 
 
-    // <div>
-    //   {
-    //     !loginSuccess ?
-    //     <LoginPage loginSuccess = {setLoginStatus}/>
-    //     :<BondsPage>
-    //   }
-    // </div>
+        // <div>
+        //   {
+        //     !loginSuccess ?
+        //     <LoginPage loginSuccess = {setLoginStatus}/>
+        //     :<BondsPage>
+        //   }
+        // </div>
 
-  );
-}
+    );
+};
 
 
 
