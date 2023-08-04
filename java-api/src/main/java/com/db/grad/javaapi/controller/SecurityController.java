5f6bf978c1bcb73541390cc1c43c4df9866a0a99
yaoviav <58631868+yaoviav@users.noreply.github.com>
2023-08-04 20:05:30 +0100

    Update SecurityController.java

--- java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http:localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SecurityController {
+
 
     @Autowired
     SecurityService securityService;
