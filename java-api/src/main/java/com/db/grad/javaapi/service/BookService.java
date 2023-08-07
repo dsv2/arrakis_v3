70013ef79d83b728c1bb14d0a7b8f78bec8fe6fa
oforiwaasam <lilyoforiwaas@gmail.com>
2023-08-07 19:33:20 +0100

    added book model with corresponding code

---
+++ java-api/src/main/java/com/db/grad/javaapi/service/BookService.java
@@ -0,0 +1,19 @@
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Book;
+import com.db.grad.javaapi.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
